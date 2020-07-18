0a2bb198a080cc1ca7e93bfe24e8dd271f30bad7
glccsi <52859745+glccsi@users.noreply.github.com>
2020-07-18 12:52:53 +0200

    merged the two py files

--- sales_data.py
+++ sales_data.py
@@ -1,15 +1,17 @@
 
 #Import Modules
-import time;
+import time
+import shutil
+import os
 
 #Global variables
-filelua = open("D:/Programming/ESOP/ArkadiusTradeToolsSalesData01.txt")
-f = open("D:/Programming/ESOP/exported_sales.txt","w")
+filelua = open("D:/Programming/pull_eso_data/ArkadiusTradeToolsSalesData01.txt")
+f = open("D:/Programming/pull_eso_data/exported_sales.csv","w")
 
 #Create a list of lines from the source file
 def write_sales():
     lines = filelua.readlines() #Creates a list which contains the lines of the source file
-    number_of_commas = 0 
+    number_of_commas = 0 #Number of commas in a line
 
     for i in range(8,len(lines)):
 
@@ -17,7 +19,8 @@ def write_sales():
             ("buyerName" in lines[i]) or ("guildName" in lines[i]) or 
             ("taxes" in lines[i])     or ("itemLink" in lines[i]) or 
             ("timeStamp" in lines[i]) or ("sellerName" in lines[i]) or 
-            ("quantity" in lines[i])  or ("price" in lines[i]) or ("internal" in lines[i])
+            ("quantity" in lines[i])  or ("price" in lines[i]) or
+            ("internal" in lines[i])
            ): 
             string = lines[i]
             len_of_line = len(string)
@@ -36,6 +39,17 @@ def close_files():
     f.close()
     filelua.close()
 
+#Open the original file, copy and paste it into program's directory
+def import_sales():
+    
+    original_data = r'C:\Users\Imi-PC\Documents\Elder Scrolls Online\live\SavedVariables\ArkadiusTradeToolsSalesData01.lua'
+    target_data = r'D:\Programming\ESOP\ArkadiusTradeToolsSalesData01.lua'
+    #call shutil module's copyfile function, to copy the original_data to target_data
+    shutil.copyfile(original_data, target_data)
+
+    #file's type from lua to txt
+    os.rename(r'D:\Programming\ESOP\ArkadiusTradeToolsSalesData01.lua',r'D:\Programming\ESOP\ArkadiusTradeToolsSalesData01'+'.txt')
+
 #Convert epoch time to human readable
 def convert_epoch_to_human():
     epoch = 1592395250
@@ -46,5 +60,5 @@ def main():
     write_sales()
     close_files()
 
-#main()
+main()
 convert_epoch_to_human()
